d7643095e52ee4927949253fc0572986110079c4
eightys3v3n <eightys3v3n@gmail.com>
2021-02-04 20:44:27 -0700

    Added documentation

--- task3/task3.ino
+++ task3/task3.ino
@@ -1,12 +1,37 @@
+/* Author: Terrence Plunkett
+ * Date: 2021-02-04
+ *
+ * Should improve upon by utilizing a game loop structure for state changes.
+ * That is, the loop should not wait while doing an action on one LED. This
+ * limits the number of concurrent changes to one LED and its inefficient.
+ * But with other classes I didn't get past designing a better loop in time.
+ */
+
+
+// Pins for the various LEDs
 const int LED_A = 9;
 const int LED_B = 10;
 const int LED_C = 11;
-const int DELAY_WAIT = 500;
+
+// How long to wait in the initial state before commensing the pattern.
+const int DELAY_START = 1000;
+
+// Increase/decrease LED brightness by this amount (0-255) when fading.
 const int FADE_INC = 10;
+
+// Wait for this many milliseconds in between FADE_INCs.
 const int FADE_DELAY = 100;
+
+// Wait for this long between state changes in a blink.
 const int BLINK_DELAY = 500;
+
+// Wait for how long before restarting the pattern.
 const int RESTART_DELAY = 4000;
 
+
+/* Fade an LED at PWM pin `id` from `start` to `end` brightness in `inc` increments with `wait` milliseconds
+ * between each increment.
+ */
 void fade(int id, int start, int end, int inc=FADE_INC, int wait=FADE_DELAY) {
   int brightness = start;
 
@@ -18,7 +43,8 @@ void fade(int id, int start, int end, int inc=FADE_INC, int wait=FADE_DELAY) {
   } else {
     return;
   }
-  
+
+  // delay, increment, write
   while (brightness != end) {
     delay(wait);
 
@@ -36,14 +62,17 @@ void fade(int id, int start, int end, int inc=FADE_INC, int wait=FADE_DELAY) {
     delay(wait);
 }
 
+// Fade an LED down from 255 to 0 brightness.
 void fade_down(int id, int inc=FADE_INC) {
   fade(id, 255, 0, inc=inc);
 }
 
+// Fade an LED up from 0 to 255 brightness.
 void fade_up(int id, int inc=FADE_INC) {
   fade(id, 0, 255, inc=inc);
 }
 
+// Blink an ON LED to OFF for wait milliseconds.
 void blink_off(int id, int wait=BLINK_DELAY) {
   digitalWrite(id, LOW);
   delay(wait);
@@ -51,14 +80,14 @@ void blink_off(int id, int wait=BLINK_DELAY) {
   delay(wait);
 }
 
-
+// Setup the initial LED state.
 void initial_leds() {
   digitalWrite(LED_A, HIGH);
   digitalWrite(LED_B, HIGH);
   digitalWrite(LED_C, LOW);
 }
 
-
+// Configure LED pins and set their initial state.
 void setup() {
   pinMode(LED_A, OUTPUT);
   pinMode(LED_B, OUTPUT);
@@ -67,9 +96,8 @@ void setup() {
   initial_leds();
 }
 
-
 void loop() {
-  delay(DELAY_WAIT);
+  delay(DELAY_START);
   fade_down(LED_B);
   delay(100);
   blink_off(LED_A);
@@ -77,6 +105,6 @@ void loop() {
   blink_off(LED_A);
   fade_up(LED_C, 5);
 
-  delay(RESTART_DELAY);
   initial_leds();
+  delay(RESTART_DELAY);
 }
